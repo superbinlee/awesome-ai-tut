b94d9bd47a4703866839c9699642684c20fa2357
xjdlb <xjd.binlee@qq.com>
2021-05-28 00:35:50 +0800

    [add]add jupyter notebook stock linear regression

--- tut_1_lazy_programmmer/ann+rnn_py_demo/ann.py
+++ tut_1_lazy_programmmer/ann+rnn_py_demo/ann.py
@@ -20,7 +20,12 @@ mnist = tf.keras.datasets.mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # X_train, y_train = X_train/255.0, y_train/255.0
 
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)), tf.keras.layers.Dense(128, activation='relu'), tf.keras.layers.Dropout(0.2), tf.keras.layers.Dense(10, activation='softmax')])
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics='accuracy')
 
